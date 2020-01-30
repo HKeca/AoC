6c67f3631091d9e3f0a018c2cc31e9c7388ad45a
Hunter Keca <hunter@heysparkbox.com>
2020-01-30 13:56:02 -0500

    test: Update test data for day one

--- tests/canary.test.js
+++
@@ -1,7 +0,0 @@
-const { expect } = require('chai');
-
-describe('Day one test', () => {
-  it('is a canary test', () => {
-    expect(true).to.be.false;
-  });
-});
---
+++ tests/day-1.test.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+const getFuelRequired = require('../day-1/index');
+
+describe('Day one - AoC', () => {
+  it('with a mass of 12 output should be two', () => {
+    const fuelReq = getFuelRequired(12);
+
+    expect(fuelReq).to.equal(2);
+  });
+
+  it('with a mass of 1969 output should be 654', () => {
+    const fuelReq = getFuelRequired(1969);
+
+    expect(fuelReq).to.equal(654);
+  });
+
+  it('with a mass of 100756 output should be 33583', () => {
+    const fuelReq = getFuelRequired(100756);
+
+    expect(fuelReq).to.equal(33583);
+  });
+});
