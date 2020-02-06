59d8e633b3fc711319e258219c29d358fc7a6e59
Hunter Keca <hunter@heysparkbox.com>
2020-02-06 11:38:53 -0500

    test: Change test for refactored getRequiredFuel

--- tests/day-1--part2.test.js
+++ tests/day-1--part2.test.js
@@ -1,21 +1,31 @@
 const { expect } = require('chai');
-const getFuelRequired = require('../day-1--part2/getFuelRequired');
+const { pipe } = require('ramda');
+const { _getFuelReqs, getFuelReqs } = require('../day-1--part2/getFuelRequired');
 
 describe('Day one (part2) - recursive fuel reqs', () => {
   it('with a mass of 1969 output should be 966', () => {
-    const fuelReq = getFuelRequired(1969);
+    const fuelReq = pipe(
+      _getFuelReqs,
+      getFuelReqs
+    )(1969);
 
     expect(fuelReq).to.equal(966);
   });
 
   it('with a mass of 100756 output should be 50346', () => {
-    const fuelReq = getFuelRequired(100756);
+    const fuelReq = pipe(
+      _getFuelReqs,
+      getFuelReqs
+    )(100756);
 
     expect(fuelReq).to.equal(50346);
   });
 
   it('with a mass of 14 output should be 2', () => {
-    const fuelReq = getFuelRequired(14);
+    const fuelReq = pipe(
+      _getFuelReqs,
+      getFuelReqs
+    )(14);
 
     expect(fuelReq).to.equal(2);
   });
