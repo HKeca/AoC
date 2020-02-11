941a3ace5be57ae1d0c9cce4f02c5499a65ce267
Hunter Keca <hunter@heysparkbox.com>
2020-02-11 09:22:55 -0500

    test (Day4): Add test cases for neverLessThan

--- tests/day-4.test.js
+++ tests/day-4.test.js
@@ -1,16 +1,31 @@
 const { expect } = require('chai');
-const findSimilar = require('../day-4/index');
+const {
+  findSimilar,
+  neverLessThan
+} = require('../day-4/index');
 
 describe('Day 4', () => {
   describe('Two adjacent digits are the same', () => {
     it('Should return true when there is one pair', () => {
-      const result = findSimilar("111111");
+      const result = findSimilar('111111');
       expect(result).to.equal(true);
     });
 
     it('Should return false when there is no pairs', () => {
       const result = findSimilar('121212');
       expect(result).to.equal(false);
-    })
+    });
+  });
+
+  describe('Next number is greater than or equal to previous number', () => {
+    it('Should return true if next number is greater than or equal to prev', () => {
+      const result = neverLessThan('123456');
+      expect(result).to.equal(true);
+    });
+
+    it('Should return false if next number is less than previous', () => {
+      const result = neverLessThan('123454');
+      expect(result).to.equal(false);
+    });
   });
 });
