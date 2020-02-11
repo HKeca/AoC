12d104465339886ae9512a378516ccd0c7abebed
Hunter Keca <hunter@heysparkbox.com>
2020-02-11 09:37:36 -0500

    refactor (Day4): Make neverLessThan functional

--- day-4/index.js
+++ day-4/index.js
@@ -1,4 +1,5 @@
 const isSimilar = (a, b) => parseInt(a) === parseInt(b);
+const lessThanEqual = (a, b) => parseInt(a) <= parseInt(b);
 
 /**
  * Find if string has at least one matching pair side by side
@@ -21,14 +22,11 @@ const findSimilar = (input, idx = 1) => (
  * @param {String} input string of numbers
  * @returns {Boolean}
  */
-const neverLessThan = input => {
-  for (let i = 1; i < input.length; i++) {
-    if (parseInt(input[i]) <= parseInt(input[i - 1]))
-      return false;
-  }
-
-  return true;
-}
+const neverLessThan = (input, idx = 1) => (
+  idx > input.length ?
+    true :
+    lessThanEqual(input[idx], input[idx - 1]) ? false : neverLessThan(input, idx + 1)
+);
 
 module.exports = {
   findSimilar,
