dfa4112d2e4d6d6bbeafb1329fb484ae5c5bf6bd
Hunter Keca <hunter@heysparkbox.com>
2020-02-12 10:13:58 -0500

    test (Day4): Update tests for previous refactor

--- tests/day-4.test.js
+++ tests/day-4.test.js
@@ -1,30 +1,30 @@
 const { expect } = require('chai');
 const {
-  findSimilar,
-  neverLessThan
+  hasMatchingPair,
+  doesntDecrease
 } = require('../day-4/index');
 
 describe('Day 4', () => {
   describe('Two adjacent digits are the same', () => {
     it('Should return true when there is one pair', () => {
-      const result = findSimilar('111111');
+      const result = hasMatchingPair('111111');
       expect(result).to.equal(true);
     });
 
     it('Should return false when there is no pairs', () => {
-      const result = findSimilar('121212');
+      const result = hasMatchingPair('121212');
       expect(result).to.equal(false);
     });
   });
 
   describe('Next number is greater than or equal to previous number', () => {
     it('Should return true if next number is greater than or equal to prev', () => {
-      const result = neverLessThan('123456');
+      const result = doesntDecrease('123456');
       expect(result).to.equal(true);
     });
 
     it('Should return false if next number is less than previous', () => {
-      const result = neverLessThan('123454');
+      const result = doesntDecrease('123454');
       expect(result).to.equal(false);
     });
   });
