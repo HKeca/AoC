014aa258bb47830d4e8a9faacd76790350883927
Hunter Keca <hunter@heysparkbox.com>
2020-02-14 10:19:56 -0500

    test (Day4-P2): Add testing for part two
    - Take in functions from their files instead of index.js

---
+++ tests/day-4--part2.test.js
@@ -0,0 +1,29 @@
+const { expect } = require('chai');
+const hasMatchingPair = require('../day-4--part2/hasMatchingPair');
+const doesntDecrease = require('../day-4--part2/doesntDecrease');
+
+describe('Day 4 - part 2', () => {
+  describe('Two adjacent digits are the same', () => {
+    it('Should return true when there is one pair', () => {
+      const result = hasMatchingPair('112222');
+      expect(result).to.equal(true);
+    });
+
+    it('Should return false when there is no pairs', () => {
+      const result = hasMatchingPair('121212');
+      expect(result).to.equal(false);
+    });
+  });
+
+  describe('Next number is greater than or equal to previous number', () => {
+    it('Should return true if next number is greater than or equal to prev', () => {
+      const result = doesntDecrease('123456');
+      expect(result).to.equal(true);
+    });
+
+    it('Should return false if next number is less than previous', () => {
+      const result = doesntDecrease('123454');
+      expect(result).to.equal(false);
+    });
+  });
+});
