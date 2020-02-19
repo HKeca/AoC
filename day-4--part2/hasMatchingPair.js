058f7941f572e57d14ca39def2885e0a36c64803
Hunter Keca <hunter@heysparkbox.com>
2020-02-19 11:43:56 -0500

    refactor (Day4): Rename checkBounds to isStrictlyPair for clarity

--- day-4--part2/hasMatchingPair.js
+++ day-4--part2/hasMatchingPair.js
@@ -1,7 +1,18 @@
 const isEqual = (a, b) => parseInt(a) === parseInt(b);
 
-// This might lead to unexpected results... not checking if "c" is undefined
-const checkBounds = (a, b, c) => parseInt(a) !== parseInt(b) && parseInt(a) !== parseInt(c);
+/**
+ * Check around pair to make sure its only a pair ("11")
+ * and not multiple same digits ("111")
+ *
+ * >b should not equal a and c should not equal a
+ *
+ * @param {String} a
+ * @param {String} b
+ * @param {String} c
+ */
+const isStrictlyPair = (a, b, c) =>
+  parseInt(a) !== parseInt(b) &&
+  parseInt(a) !== parseInt(c);
 
 /**
  * Find if string has at least one matching pair side by side
@@ -16,6 +27,7 @@ const hasMatchingPair = (input, idx = 1) => (
   idx > input.length
   ? false
   : isEqual(input[idx], input[idx - 1])
+      && isStrictlyPair(input[idx], input[idx - 2], input[idx + 1])
   ? true
   : hasMatchingPair(input, idx + 1)
 );
